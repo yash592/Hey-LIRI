f9c976c417f81aafc7400c85ec9215e06b6c261d
Yash <yash592@gmail.com>
2017-11-19 16:08:41 -0800

    Prompts done. Working on tweets

---
+++ keys.js
@@ -0,0 +1,25 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'bnICMoNqPDvV83mppQYCD3JJm',
+  consumer_secret: 'n3q6bNOx72P7mW4l83EhHVD5ff925fJBxI4f4MtraMA4lSsncR',
+  access_token_key: '932310531783794688-zUnUejjA6qPa6v8f52moAKPixhDHJRG',
+  access_token_secret: 'B1cc1YyrUWn7I7mmjJ0mYUmGVQx2Dfze23iKttoiL4N09',
+};
+
+// // Client ID Spotify
+// 5cce13d7106545b9aa085dd5f5c9598f
+// Client Secret
+// 30ffbb31f6164884befe7aa462307994
+
+var spotifyKeys = {
+	id: '5cce13d7106545b9aa085dd5f5c9598f',
+	secret: '30ffbb31f6164884befe7aa462307994'
+};
+
+
+
+module.exports = {
+	spotifyKeys: spotifyKeys,
+	twitterKeys: twitterKeys
+}
---
+++ liri.js
@@ -0,0 +1,57 @@
+var keys = require("./keys.js");
+var fs = require("fs");
+var inquirer = require("inquirer");
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+var omdb = require("omdbapi");
+
+var client = new twitter({
+  consumer_key: 'bnICMoNqPDvV83mppQYCD3JJm',
+  consumer_secret: 'n3q6bNOx72P7mW4l83EhHVD5ff925fJBxI4f4MtraMA4lSsncR',
+  access_token_key: '932310531783794688-zUnUejjA6qPa6v8f52moAKPixhDHJRG',
+  access_token_secret: 'B1cc1YyrUWn7I7mmjJ0mYUmGVQx2Dfze23iKttoiL4N09',
+});
+
+
+
+// Prompting user about what they want
+
+inquirer.prompt ([
+
+
+{
+	type: "list",
+	name: "whatyawant",
+	message: "What do you want me to to?",
+	choices: ["Show me my tweets", "Spotify a song", "Show me a movie", "Do what it says"]
+},
+
+    // switch cases to evaluate and respong to user's input
+
+	]).then(function(response) {
+
+		switch (response.whatyawant) {
+			case "Show me my tweets":
+			// tweets();
+			console.log("tweets!");
+			break;
+
+			case "Spotify a song":
+			console.log("songs!");
+			break;
+
+			case "Show me a movie":
+			console.log("movies!");
+			break;
+
+			case "Do what it says":
+			console.log("FU!");
+			break;
+
+		}
+
+		// @DonalIdTrump show tweets
+
+		
+
+	});
